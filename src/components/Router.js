7f99118b84753892257bdf3a5626112524125524
pablokimm <pablo@xreators.com>
2022-10-17 16:06:18 +0900

    1.0.8 UI Update - Log-in page

--- src/components/Router.js
+++ src/components/Router.js
@@ -15,20 +15,28 @@ const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
       {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
-          <>
+          <div>
+            style={{
+              maxWidth: 890,
+              width: "100%",
+              margin: "0 auto",
+              marginTop: 80,
+              display: "flex",
+              justifyContent: "center",
+            }}
             <Route exact path="/">
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
               <Profile refreshUser={refreshUser} userObj = {userObj} />
             </Route>
-          </>
+          </div>
         ) : (
-          <>
+          <div>
             <Route exact path="/">
               <Auth />
             </Route>
-          </>
+          </div>
         )}
       </Switch>
     </Router>
